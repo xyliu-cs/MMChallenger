ce44ec4f9aefec25e9f847f3f2c23a7e8ac22fef
xiaoyuanliu <xiaoyuanliu@link.cuhk.edu.cn>
2024-03-01 23:29:58 +0800

    optimize ppl calculation logic

--- model_ppl/calculate_perplexity.py
+++ model_ppl/calculate_perplexity.py
@@ -2,37 +2,38 @@ import torch
 from transformers import LlamaForCausalLM, LlamaTokenizer, AutoTokenizer, AutoModelForCausalLM
 import argparse
 import json
-
+from datetime import date
+ 
+# Returns the current local date
+today = date.today()
+print("Today date is: ", today)
 
 parser = argparse.ArgumentParser(description='Preprocess the input files.')
 parser.add_argument('model_file_path', type=str, help='The path to the hf llama-2 model and tokenizer folder')
 parser.add_argument('input_file_path', type=str, help='The path to the compositional sentences json file')
-parser.add_argument('--output_file_path', type=str, default='sentence_lookups.json' , help='The path to the output file for sentence ppl lookup')
+# parser.add_argument('--output_file_path', type=str, default='sentence_lookups.json' , help='The path to the output file for sentence ppl lookup')
 
 # Execute the parse_args() method
 args = parser.parse_args()
 model_file_path = args.model_file_path
 input_file_path = args.input_file_path
-output_file_path = args.output_file_path
+# output_file_path = args.output_file_path
 
 
 def calculate_perplexity(sentence, tokenizer, model, device):
-    model.eval() 
+    model.eval()
     # Tokenize the input sentence
     encodings = tokenizer(sentence, return_tensors="pt")
-
     input_ids = encodings.input_ids.to(device)
-    target_ids = input_ids.clone()
     attention_mask = encodings.attention_mask.to(device)
 
-    # Calculate Negative Log-Likelihood
+    # Calculate Negative Log-Likelihood with no_grad context
     with torch.no_grad():
-        outputs = model(input_ids, labels=target_ids, attention_mask=attention_mask)
-        neg_log_likelihood = outputs.loss * input_ids.size(1)  # Multiply by sequence length
+        outputs = model(input_ids, labels=input_ids, attention_mask=attention_mask)
+        neg_log_likelihood = outputs.loss
 
-    # Calculate Perplexity and append to list
-    ppl = torch.exp(neg_log_likelihood / input_ids.size(1))  # Divide by sequence length
-    perplexity = ppl.item()
+    # Calculate Perplexity directly from NLL
+    perplexity = torch.exp(neg_log_likelihood).item()
 
     return perplexity
 
@@ -43,9 +44,13 @@ model_folder_name = model_file_path.split('/')[-1].lower()
 if "llama2" in model_folder_name:
     tokenizer = LlamaTokenizer.from_pretrained(model_file_path)
     model = LlamaForCausalLM.from_pretrained(model_file_path).to(device)
+    output_file_path = f"/120040051/test_resource/sentence_ppl_lookup/sentence_lookup_llama2_{today}.json"
+    
 elif "vicuna" in model_folder_name:
     tokenizer = AutoTokenizer.from_pretrained(model_file_path)
     model = AutoModelForCausalLM.from_pretrained(model_file_path).to(device)
+    output_file_path = f"/120040051/test_resource/sentence_ppl_lookup/sentence_lookup_vicuna_{today}.json"
+
 else:
     print(f"Language model {model_folder_name} is not supported yet.")
 
