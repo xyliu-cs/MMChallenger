2b3b3de68955965248cd81d77f8ef597f6f0710b
xiaoyuanliu <liuxiaoyuan2001@gmail.com>
2024-07-29 18:17:39 +0800

    add human eval interface

--- eval_model/detect_errors.py
+++ eval_model/detect_errors.py
@@ -1,4 +1,7 @@
 from eval import read_json, write_json
+from templates import OPENAI_CHECK_ANS_PREFIX_ACTION
+from tqdm import tqdm
+import json, os, time, requests, datetime
 
 def check_mcq_correctness(model_ans, label_ans):
     sanity_list = [label_ans, f'({label_ans})']
@@ -9,6 +12,58 @@ def check_mcq_correctness(model_ans, label_ans):
 
 def check_yn_correctness(model_ans, label_ans):
     return label_ans.lower() in model_ans.lower()
+
+# Result is not satisfying !!!
+# def check_sa_correctness_auto(basic_str, prefix):
+#     text_prompt = prefix + basic_str
+#     # print(basic_str)
+#     url = "https://aigptx.top/v1/chat/completions"
+#     api_key = os.environ.get('MY_API_KEY')
+#     payload = json.dumps({
+#         "model": "gpt-4o-mini",
+#         "messages": [
+#             {
+#                 "role": "system",
+#                 "content": "You are a helpful assistant."
+#             },
+#             {
+#                 "role": "user",
+#                 "content": text_prompt
+#             }
+#         ]
+#     })
+#     headers = {
+#         'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
+#         'Content-Type': 'application/json',
+#         "Authorization": f"Bearer {api_key}"
+#     }
+
+#     attempt = 0
+#     while attempt < 3:
+#         try:
+#             response = requests.request("POST", url, headers=headers, data=payload)
+#             response.raise_for_status()  # Raises exception for HTTP errors
+#             res_dict = response.json()
+#             result = res_dict['choices'][0]['message']['content']
+#             # print(result)
+#             break
+#         except Exception as e:
+#             print(f"Attempt {attempt + 1} failed: {str(e)}")
+#             time.sleep(5)  # Wait before retrying
+#             attempt += 1
+#     if attempt >= 3:
+#         print("Maximum retry attempts reached, giving up.")
+#         return "Error"
+        
+#     time.sleep(2)
+#     return result
+
+def check_sa_correctness_human(basic_str):
+    print(basic_str)
+    eval_result = input("Enter 0 for incorrect, 1 for partially correct, 2 for correct. Separate with white space.\n")
+    while eval_result not in ['0', '1', '2']:
+        eval_result = input("Invalid input. Please enter 0 for incorrect, 1 for partially correct, 2 for correct. Separate with white space.\n")
+    return eval_result
 
 
 def find_mcq_errors(output_list: list, target="all") -> list:
@@ -79,27 +134,104 @@ def find_yn_errors(output_list: list, target="all") -> list:
     return ret_list
 
 
+# def standardize_status(input_string):
+#     keywords = ["Correct", "Acceptable", "Incorrect"]
+#     for keyword in keywords:
+#         if keyword in input_string:
+#             return keyword
+
+
+def find_sa_errors(output_list: list, target="all", eval_type="human") -> list:
+    assert target in ["all", "action", "place"]
+    ret_list = []
+    if target == "all":
+        eval_output_list = output_list
+    elif target == "action":
+        eval_output_list = [output_dict for output_dict in output_list if output_dict["category"] == "action"]
+    elif target == "place":
+        eval_output_list = [output_dict for output_dict in output_list if output_dict["category"] == "place"]
+
+    if eval_type == "human":
+        evaler = input("Please enter your name in english: \n")
+        eval_email = input("Please enter your email address: \n")
+        start_time = datetime.datetime.now()
+
+    for idx, output_dict in enumerate(eval_output_list[:]):
+        total = len(eval_output_list)
+        id = output_dict["id"]
+        category = output_dict["category"]
+        model_ans_lists = output_dict["sa_model_ans"]
+        local_dict = {"id": id, "category": category, "image": [], "info": '',"human_eval": []}
+        ans_str_list = []
+        for iid, ans_list in enumerate(model_ans_lists):
+            ans_set = list(set(ans_list))
+            ans_string = f"[Model answer {iid+1}] {', '.join(ans_set)}"
+            ans_str_list.append(ans_string)
+        total_ans_str = '\n'.join(ans_str_list)
+        # rebuild clean question string
+        mcq_str = output_dict["mcq"]
+        ques_str = "[Question] " + mcq_str.split("Question:")[1].split("Options:")[0].strip()
+        ground_truth = "[Ground truth] " + list(output_dict["target"].values())[0]
+        basics = f"{ques_str}\n{ground_truth}\n{total_ans_str}"
+        result = check_sa_correctness_human(basics)
+        results = result.split(' ')
+        for i, result_digit in enumerate(results):
+            local_dict["image"].append(i+1)
+            local_dict["info"] = basics
+            local_dict["human_eval"].append(result_digit)
+        ret_list.append(local_dict)
+        print(f"Progress: {idx+1}/{total} labeled.")
+        print("\n")
+
+    end_time = datetime.datetime.now()
+    ret_list.append({"evaluator": evaler, "evaluator_email": eval_email, "start_time": str(start_time), "end_time": str(end_time), "time_used": str(end_time - start_time)})
+    return ret_list
+
+
 if __name__ == "__main__":
     # output_path = "/120040051/test_resource/output_answers/llava15_7b_outputs_fmt.json"
-    output_path = "/120040051/test_resource/output_answers/instructblip_7b_outputs.json"
+    # output_path = "/120040051/test_resource/output_answers/instructblip_7b_outputs.json"
+    # output_path = "/home/liu/github_repos/MMChallenger/eval_model/llava15_13b_outputs.json"
 
-    outputs = read_json(output_path)
-    mcq_error_list = find_mcq_errors(outputs)
-    yn_error_list = find_yn_errors(outputs)
 
+    # # outputs = read_json(output_path)
+    # # mcq_error_list = find_mcq_errors(outputs)
+    # # yn_error_list = find_yn_errors(outputs)
+    # sa_eval_list = find_sa_errors(output_list=outputs, target="all")
+    # write_json("llava15_13b_sa_human_eval.json", sa_eval_list)
+
+
+    eval_res = read_json("llava15_13b_sa_human_eval.json")
+    total = 0
+    action = 0
+    place = 0
+    assert len(eval_res) == 345
+    for eval_dict in eval_res[:-1]:
+        if eval_dict["human_eval"][0] == "0":
+            total += 1
+            if eval_dict["category"] == "action":
+                action += 1
+            elif eval_dict["category"] == "place":
+                place += 1
+    print("sa all: ", total)
+    print("sa action: ", action)
+    print("sa place: ", place)
+
+        
+
+    # # print(mcq_error_list)
+    # print("mcq all: ", len(mcq_error_list))
+    # mcq_error_list_action = find_mcq_errors(outputs, target="action")
+    # print("mcq action: ", len(mcq_error_list_action))
+    # mcq_error_list_place = find_mcq_errors(outputs, target="place")
+    # print("mcq place: ", len(mcq_error_list_place))
+
+    # # print(yn_error_list)
+    # print("yn all: ", len(yn_error_list))
+    # yn_error_list_action = find_yn_errors(outputs, target="action")
+    # print("yn action: ", len(yn_error_list_action))
+    # yn_error_list_place = find_yn_errors(outputs, target="place")
+    # print("yn place: ", len(yn_error_list_place))
+
+    # print('\n')
     # print(mcq_error_list)
-    print("mcq all: ", len(mcq_error_list))
-    mcq_error_list_action = find_mcq_errors(outputs, target="action")
-    print("mcq action: ", len(mcq_error_list_action))
-    mcq_error_list_place = find_mcq_errors(outputs, target="place")
-    print("mcq place: ", len(mcq_error_list_place))
-
-    # print(yn_error_list)
-    print("yn all: ", len(yn_error_list))
-    yn_error_list_action = find_yn_errors(outputs, target="action")
-    print("yn action: ", len(yn_error_list_action))
-    yn_error_list_place = find_yn_errors(outputs, target="place")
-    print("yn place: ", len(yn_error_list_place))
-
-    print('\n')
-    print(mcq_error_list)
