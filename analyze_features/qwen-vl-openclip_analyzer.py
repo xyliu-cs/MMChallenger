eb209f25184a3bb4d134f1f2b6fb2717d4d448ae
xiaoyuanliu <xiaoyuanliu@link.cuhk.edu.cn>
2024-08-31 17:58:12 +0800

    first eol attempt

--- analyze_features/qwen-vl-openclip_analyzer.py
+++ analyze_features/qwen-vl-openclip_analyzer.py
@@ -19,10 +19,13 @@ class qwen_vl_feature_analyzer:
         
         self.open_clip = config['open_clip_path']
         self.qwen_vl_path = config['qwen_vl_path']
+        
+        self.action_exp = config['action_exp']
+        self.place_exp = config['place_exp']
 
-        self.vision_features = []
-        self.adaptor_features = []
-        self.language_features = []
+        # self.vision_features = []
+        # self.adaptor_features = []
+        # self.language_features = []
         
     def prepare_input_pairs(self) -> None:
         index_file_path = os.path.join(self.challset_folder, self.index_file)
@@ -137,36 +140,52 @@ class qwen_vl_feature_analyzer:
             scored_list.append(ret_tup)
         return scored_list
 
+
     def compare_qwen_emb(self, image_text_pairs, pooling_type='avg') -> list:
         
         qwen_vl = AutoModelForCausalLM.from_pretrained(self.qwen_vl_path, trust_remote_code=True, fp16=True, device_map='auto')
         tokenizer = AutoTokenizer.from_pretrained(self.qwen_vl_path, trust_remote_code=True)
         tokenizer.pad_token_id = tokenizer.eod_id
-        tokenizer.padding_side = 'right'
+        tokenizer.padding_side = 'left'
         
         scored_list = []
         for image_text_tup in tqdm(image_text_pairs[:], "Checking qwen embeddings: "):
-            text_caps = [image_text_tup[2], image_text_tup[3], image_text_tup[4]]      # [true, false, dummy]
+            text_caps = [cap.capitalize() + '.' for cap in image_text_tup[2:]]      # [true, false, dummy]
 
-            img_query = f"<img>{image_text_tup[0]}</img>"
-            img_inputs = tokenizer(img_query, return_tensors='pt').to(qwen_vl.device)
+            # TODO: add some logic to distinguish the question type
+            prompt_eol = 'This sentence : "{sent_exp}" means in one word:"{word_exp}". This sentence : "{actual_sent}" means in one word:"'
+            img_seq = f"<img>{image_text_tup[0]}</img>"
             
-            text_inputs = tokenizer(text_caps, padding=True, return_tensors="pt").to(qwen_vl.device)
+            if image_text_tup['type'] == 'action':
+                image_query = prompt_eol.format(sent_exp=self.action_exp[0], word_exp=self.action_exp[1], actual_sent=img_seq)
+                text_query = [prompt_eol.format(sent_exp=self.action_exp[0], word_exp=self.action_exp[1], actual_sent=text_cap) for text_cap in text_caps]
+            elif image_text_tup['type'] == 'place':
+                image_query = prompt_eol.format(sent_exp=self.place_exp[0], word_exp=self.place_exp[1], actual_sent=img_seq)
+                text_query = [prompt_eol.format(sent_exp=self.place_exp[0], word_exp=self.place_exp[1], actual_sent=text_cap) for text_cap in text_caps]  
+            
+            img_inputs = tokenizer(image_query, return_tensors='pt').to(qwen_vl.device)
+            text_inputs = tokenizer(text_query, padding=True, return_tensors="pt").to(qwen_vl.device)
+            
             # text_input_ids = text_inputs.input_ids
             with torch.no_grad():
                 qwen_img_features = qwen_vl(**img_inputs, output_hidden_states=True)
                 qwen_text_features = qwen_vl(**text_inputs, output_hidden_states=True)
                 img_lhs = qwen_img_features.hidden_states[-1]
                 text_lhs = qwen_text_features.hidden_states[-1]
-                # Apply pooling to the embeddings
-                if pooling_type == 'avg':
-                    pooled_image_embeds = torch.mean(img_lhs, dim=1)  
-                    pooled_text_embeds = torch.mean(text_lhs, dim=1)
-                elif pooling_type == 'max':
-                    pooled_image_embeds = torch.max(img_lhs, dim=1)[0]  
-                    pooled_text_embeds = torch.max(text_lhs, dim=1)[0]
-                else:
-                    raise ValueError("Invalid pooling type. Use 'max' or 'avg'.")                
+
+            # Apply pooling to the embeddings
+            if pooling_type == 'avg':
+                pooled_image_embeds = torch.mean(img_lhs, dim=1)  
+                pooled_text_embeds = torch.mean(text_lhs, dim=1)
+            elif pooling_type == 'max':
+                pooled_image_embeds = torch.max(img_lhs, dim=1)[0]  
+                pooled_text_embeds = torch.max(text_lhs, dim=1)[0]
+            elif pooling_type == 'eol':
+                pooled_image_embeds = img_lhs[:, -1, :]
+                # text is right aligned since we pad to the left
+                pooled_text_embeds = text_lhs[:, -1, :] 
+            else:
+                raise ValueError("Invalid pooling type. Use 'max' or 'avg'.")                
                 
             cosine_sim = utils.cosine_sim(pooled_image_embeds, pooled_text_embeds)
             # Append pooled features
@@ -178,39 +197,39 @@ class qwen_vl_feature_analyzer:
     def __call__(self):
         # self.prepare_input_pairs()
         input_list = utils.read_json(os.path.join(self.challset_folder, self.prepared_inputs))
-        clip_emb_list = self.compare_clip_emb(input_list, pooling_type='tok')
-        # print(clip_emb_list[:5])
-        print("Stage 1 (CLIP) feature analysis completed.")
-        torch.cuda.empty_cache()
-        pooling_type = 'avg'
+        # clip_emb_list = self.compare_clip_emb(input_list, pooling_type='tok')
+        # # print(clip_emb_list[:5])
+        # print("Stage 1 (CLIP) feature analysis completed.")
+        # torch.cuda.empty_cache()
         
-        adapter_emb_list = self.compare_adapter_emb(input_list, pooling_type=pooling_type)
-        print(f"Stage 2 (Adapter) feature analysis completed with {pooling_type} pooling.")
-        # print(adapter_emb_list[:5])
-        torch.cuda.empty_cache()
+        pooling_type = 'eol'
+        # adapter_emb_list = self.compare_adapter_emb(input_list, pooling_type=pooling_type)
+        # print(f"Stage 2 (Adapter) feature analysis completed with {pooling_type} pooling.")
+        # # print(adapter_emb_list[:5])
+        # torch.cuda.empty_cache()
 
         qwen_emb_list = self.compare_qwen_emb(input_list, pooling_type=pooling_type)
         print(f"Stage 3 (Qwen) feature analysis completed with {pooling_type} pooling.")
-        # print(qwen_emb_list[:5])
-        torch.cuda.empty_cache()
-        # everything is fine, pack and go
-        if len(clip_emb_list) == len(adapter_emb_list) == len(qwen_emb_list):
-            ret_list = []
-            for i in range(len(clip_emb_list)):
-                local_dict = {}
-                clip_info_list = clip_emb_list[i]
-                local_dict["image"] = clip_info_list[0]
-                local_dict["true_cap"] = clip_info_list[1]
-                local_dict["false_cap"] = clip_info_list[2]
-                local_dict["dummy_cap"] = clip_info_list[3]
+        print(qwen_emb_list[:20])
+        # torch.cuda.empty_cache()
+        # # everything is fine, pack and go
+        # if len(clip_emb_list) == len(adapter_emb_list) == len(qwen_emb_list):
+        #     ret_list = []
+        #     for i in range(len(clip_emb_list)):
+        #         local_dict = {}
+        #         clip_info_list = clip_emb_list[i]
+        #         local_dict["image"] = clip_info_list[0]
+        #         local_dict["true_cap"] = clip_info_list[1]
+        #         local_dict["false_cap"] = clip_info_list[2]
+        #         local_dict["dummy_cap"] = clip_info_list[3]
 
-                local_dict["clip_sim_score"] = clip_info_list[4]
-                local_dict[f"adapter_sim_score_{pooling_type}"] = adapter_emb_list[i][4]
-                local_dict[f"qwen_sim_score_{pooling_type}"] = qwen_emb_list[i][4]
+        #         local_dict["clip_sim_score"] = clip_info_list[4]
+        #         local_dict[f"adapter_sim_score_{pooling_type}"] = adapter_emb_list[i][4]
+        #         local_dict[f"qwen_sim_score_{pooling_type}"] = qwen_emb_list[i][4]
                 
-                ret_list.append(local_dict)
-            output = os.path.join(self.challset_folder, f'qwen_vl_feature_sim_score_{pooling_type}_pooling.json')
-            utils.write_json(output, ret_list)
+        #         ret_list.append(local_dict)
+        #     output = os.path.join(self.challset_folder, f'qwen_vl_feature_sim_score_{pooling_type}_pooling.json')
+        #     utils.write_json(output, ret_list)
         # else:
         #     pass #TODO: finish the logic here
     
